52df74b3caf95ce530f6d249fd8446424b922545
maksimn <maksim1989sovunion@yandex.ru>
2021-09-24 21:16:04 +0300

    [iOS-Demo-1]: Худой лэйаут всех ячеек таблицы.

--- PlatformDemo/TableView/MyTableView.swift
+++ PlatformDemo/TableView/MyTableView.swift
@@ -9,24 +9,33 @@ import UIKit
 
 class MyTableView: UITableView {
 
-    var calculated: Bool = false
+    var yOffsets: [CGFloat] = []
+    var totalHeight: CGFloat = .zero
 
     override func layoutSubviews() {
-        if calculated { return }
-
-        var prevMaxY: CGFloat = 0
-
-        for i in 0..<rowCount {
-            guard let cell = cellForRow(at: IndexPath(row: i, section: 0)) as? TableCell else { continue }
-
-            cell.frame.size = cell.intrinsicContentSize
-            cell.frame.origin = CGPoint(x: 0, y: prevMaxY)
-            prevMaxY += cell.frame.size.height
+        if yOffsets.count == 0 {
+            yOffsets = Array(repeating: .zero, count: rowCount + 1)
         }
 
-        contentSize = CGSize(width: frame.width, height: prevMaxY)
+        let paths = indexPathsForVisibleRows ?? []
+        let start = paths.first?.row ?? -1
+        let end = paths.last?.row ?? -1
 
-        calculated = true
+        for i in start...end {
+            guard let cell = cellForRow(at: IndexPath(row: i, section: 0)) as? TableCell else { continue }
+
+            let height = cell.intrinsicContentSize.height
+
+            cell.frame.size = CGSize(width: frame.width, height: height)
+            cell.frame.origin = CGPoint(x: 0, y: yOffsets[i] - contentOffset.y)
+
+            if yOffsets[i + 1].isZero {
+                yOffsets[i + 1] = yOffsets[i] + cell.frame.size.height
+                totalHeight = max(totalHeight, yOffsets[i + 1])
+            }
+        }
+
+        contentSize = CGSize(width: frame.width, height: totalHeight - contentOffset.y)
     }
 
     var rowCount: Int {
